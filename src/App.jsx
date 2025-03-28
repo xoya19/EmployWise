f4ff39380f4fcc8deda0d0b33727d914a12f1716
Zoya Ahmed <icon54008@gmail.com>
2025-03-29 02:01:38 +0530

    app change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../components/Login";
 import Users from "../components/Users";
 
 export default function App() {
   return (
-    <Router basename="/EmployWise">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/users" element={<Users />} />
+    </Routes>
   );
 }
